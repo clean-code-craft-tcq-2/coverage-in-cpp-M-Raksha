bbd5eeaa7984ae7f68fa7f7c796ef1b248b39ce1
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-02-22 16:13:08 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,4 +5,26 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+TEST_CASE("classifies and infers temperature breachs accoring to coolingtype ")
+{
+  BatteryCharacter BatteryChar;
+  BatteryChar.coolingType = PASSIVE_COOLING;
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,-12) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,40) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,25) == NORMAL);
+ 
+   BatteryChar.coolingType = HI_ACTIVE_COOLING;
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,-12) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,56) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLINGe,25) == NORMAL);
+  
+  BatteryChar.coolingType = MED_ACTIVE_COOLING;
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,-12) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,42) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,25) == NORMAL);
+ 
 }
