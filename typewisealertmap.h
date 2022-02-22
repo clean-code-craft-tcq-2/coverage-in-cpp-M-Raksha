4a945d8a3e87f1ffe0b6596e23186e0398378b01
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-02-22 15:27:03 +0530

    Update typewisealertmap.h

--- typewisealertmap.h
+++ typewisealertmap.h
@@ -1,1 +1,25 @@
+#include <vector>
+#include <map>
+using std::map;
 
+map<CoolingType, std::vector<int>>mapTempratureLimtsToCoolingtype
+{
+    {PASSIVE_COOLING, {0,35}},
+    {HI_ACTIVE_COOLING, {0,45}},
+    {MED_ACTIVE_COOLING, {0,40}}
+    
+};
+
+
+map<AlertTarget, void (*)(BreachType)> callAlertFunctionfromTargetType 
+{
+{TO_CONTROLLER, sendToController},
+{TO_EMAIL, sendToEmail}
+};
+
+map <BreachType, const char * > BreachTypeToAlertMsgMap
+{
+    {NORMAL, "all is well"},
+    {TOO_LOW, "the temperature is too low"},
+    {TOO_HIGH, "the temperature is too high"}
+};
