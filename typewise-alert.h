5a4ffac905dbf0de2b432f1cbdb2a1c87e30afbe
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-02-22 15:26:39 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <map>
-using std::map;
-
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -35,24 +31,3 @@ void checkAndAlert(
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 
-map<CoolingType, std::vector<int>>mapTempratureLimtsToCoolingtype
-{
-    {PASSIVE_COOLING, {0,35}},
-    {HI_ACTIVE_COOLING, {0,45}},
-    {MED_ACTIVE_COOLING, {0,40}}
-    
-};
-
-
-map<AlertTarget, void (*)(BreachType)> callAlertFunctionfromTargetType 
-{
-{TO_CONTROLLER, sendToController},
-{TO_EMAIL, sendToEmail}
-};
-
-map <BreachType, const char * > BreachTypeToAlertMsgMap
-{
-    {NORMAL, "all is well"},
-    {TOO_LOW, "the temperature is too low"},
-    {TOO_HIGH, "the temperature is too high"}
-};
